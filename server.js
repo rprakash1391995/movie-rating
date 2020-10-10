ef970111cd2f1c0fd38e6bb5f73a96388672446f
Ravi Prakash <rprakash1391995@gmail.com>
2020-10-10 22:17:13 +0530

    results in new page

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express');
-const cors = require('cors');
+const axios = require('axios');
+const bodyParser = require('body-parser');
 var app = express();
 
-app.use(cors());
 
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get("/", (req, res) => {
     res.render("index");
@@ -17,6 +18,17 @@ app.get('/about', (req, res) => {
 
 app.get("/posters", (req, res) => {
     res.render("posters");
+})
+
+app.post("/results", async (req, res) => {
+    console.log(req.body);
+    try {
+        const result = await axios.get(`https://www.omdbapi.com/?s=${req.body.movie}&apikey=47a0db19`);
+        console.log(result.data.Search);
+        res.render('results', {details: result.data.Search});
+    } catch (error) {
+console.log(error);
+    }
 })
 
 var port = process.env.PORT || 3000;
